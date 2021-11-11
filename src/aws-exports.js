020584de52256274f9684bfe80f2feb378ade230
Janam Parikh <janamparikh08@gmail.com>
2021-11-10 18:29:33 -0600

    added image functionality

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -28,7 +28,9 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://fmdpoofflbcidkctsktytfmr6u.appsync-api.us-east-2.amazonaws.com/graphql",
     "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3j7p5p4dpzhthngpl2pal23yna"
+    "aws_appsync_apiKey": "da2-3j7p5p4dpzhthngpl2pal23yna",
+    "aws_user_files_s3_bucket": "testbucket215429-staging",
+    "aws_user_files_s3_bucket_region": "us-east-2"
 };
 
 
